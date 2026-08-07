e90a3c52319e48dd5d8605f384c1b6d3386af5fb
Lars Moelleken <lars@moelleken.org>
2026-08-07 21:44:32 +0200

    refactor(workflow): remove session bridge from plan and approve

--- src/Workflow/WorkflowCli.php
+++ src/Workflow/WorkflowCli.php
@@ -26,8 +26,8 @@ final readonly class WorkflowCli
 
         return match ($command) {
             'help', '--help', '-h', '' => $this->printHelp(),
-            'plan' => (new WorkflowPlanCommand($this->rootPath, $this->sessionRunner))->run($rest),
-            'approve' => (new WorkflowApproveCommand($this->rootPath, $this->sessionRunner, $this->recallRunner))->run($rest),
+            'plan' => (new WorkflowPlanCommand($this->rootPath))->run($rest),
+            'approve' => (new WorkflowApproveCommand($this->rootPath, $this->recallRunner))->run($rest),
             'start' => (new WorkflowStartCommand($this->rootPath, $this->recallRunner))->run($rest),
             'status' => (new WorkflowStatusCommand($this->rootPath))->run($rest),
             'manifest' => (new WorkflowManifestCommand($this->rootPath))->run($rest),
