4c82d4d16ed79d0e95ddaa6e2c2f81f86b98b578
Lars Moelleken <lars@moelleken.org>
2026-08-13 16:11:59 +0200

    Pin slop-scan review to verified 0.1.6 PHAR

--- tools/slop-scan/slop-scan.php
+++ tools/slop-scan/slop-scan.php
@@ -3,34 +3,31 @@
 
 declare(strict_types=1);
 
-$toolRoot = __DIR__;
-$command = sprintf(
-    'composer update voku/slop-scan --with-all-dependencies --working-dir=%s --no-interaction --prefer-dist --no-progress --no-ansi',
-    escapeshellarg($toolRoot),
-);
-passthru($command, $updateExit);
-if ($updateExit !== 0) {
-    fwrite(\STDERR, "Failed to refresh isolated slop-scan lock.\n");
-    exit($updateExit);
-}
+const SLOP_SCAN_VERSION = '0.1.6';
+const SLOP_SCAN_PHAR_SHA256 = 'fde0a1e5c1789a06db56f88ab3014aec4aba44617f0181ebae6e0d7ece86c314';
+const SLOP_SCAN_PHAR_URL = 'https://github.com/voku/slop-scan/releases/download/0.1.6/slop-scan.phar';
 
-$lock = $toolRoot . '/composer.lock';
-if (in_array('--json', $argv, true)) {
-    $contents = file_get_contents($lock);
+$phar = sys_get_temp_dir() . '/voku-slop-scan-' . SLOP_SCAN_VERSION . '-' . SLOP_SCAN_PHAR_SHA256 . '.phar';
+if (!is_file($phar) || hash_file('sha256', $phar) !== SLOP_SCAN_PHAR_SHA256) {
+    $contents = file_get_contents(SLOP_SCAN_PHAR_URL);
     if (!is_string($contents)) {
-        fwrite(\STDERR, "Could not read generated slop-scan lock.\n");
+        fwrite(\STDERR, "Could not download slop-scan " . SLOP_SCAN_VERSION . ".\n");
         exit(1);
     }
-    echo $contents;
-    exit(0);
+    if (hash('sha256', $contents) !== SLOP_SCAN_PHAR_SHA256) {
+        fwrite(\STDERR, "Downloaded slop-scan PHAR failed SHA-256 verification.\n");
+        exit(1);
+    }
+    if (file_put_contents($phar, $contents, LOCK_EX) !== strlen($contents)) {
+        fwrite(\STDERR, "Could not persist verified slop-scan PHAR.\n");
+        exit(1);
+    }
 }
 
-$autoload = $toolRoot . '/vendor/autoload.php';
-if (!is_file($autoload)) {
-    fwrite(\STDERR, "Run: composer install --working-dir=tools/slop-scan\n");
-    exit(1);
+$arguments = array_map('escapeshellarg', array_slice($argv, 1));
+$command = escapeshellarg(PHP_BINARY) . ' ' . escapeshellarg($phar);
+if ($arguments !== []) {
+    $command .= ' ' . implode(' ', $arguments);
 }
-
-require $autoload;
-
-exit((new \SlopScan\Console\SlopScanApplication())->run());
+passthru($command, $exitCode);
+exit($exitCode);
