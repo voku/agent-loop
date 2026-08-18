1caec3d7703ad20fd7e00b4c7ee09162ff2918aa
Lars Moelleken <lars@moelleken.org>
2026-08-18 09:15:11 +0200

    Keep front-door stdout discard filter non-blocking

--- src/Workflow/StdoutDiscardFilter.php
+++ src/Workflow/StdoutDiscardFilter.php
@@ -18,6 +18,9 @@ final class StdoutDiscardFilter extends php_user_filter
     {
         while (($bucket = stream_bucket_make_writeable($in)) !== false) {
             $consumed += $bucket->datalen;
+            $bucket->data = '';
+            $bucket->datalen = 0;
+            stream_bucket_append($out, $bucket);
         }
 
         return PSFS_PASS_ON;
