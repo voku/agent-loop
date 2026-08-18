657b236f08634d0f7c3ad03e98bcc9d4dea660bd
Lars Moelleken <lars@moelleken.org>
2026-08-18 09:07:09 +0200

    Add front-door stdout discard filter

---
+++ src/Workflow/StdoutDiscardFilter.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace voku\AgentLoop\Workflow;
+
+use php_user_filter;
+
+/** @internal Used only while a nested owner CLI runs behind a structured front door. */
+final class StdoutDiscardFilter extends php_user_filter
+{
+    /**
+     * @param resource $in
+     * @param resource $out
+     * @param int $consumed
+     */
+    public function filter($in, $out, &$consumed, bool $closing): int
+    {
+        while (($bucket = stream_bucket_make_writeable($in)) !== false) {
+            $consumed += $bucket->datalen;
+        }
+
+        return PSFS_PASS_ON;
+    }
+}
