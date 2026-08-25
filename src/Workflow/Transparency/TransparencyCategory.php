d237eb48601aafa0f6be2efc50f59fbfd42b143f
Lars Moelleken <lars@moelleken.org>
2026-08-25 23:08:44 +0200

    fix(transparency): preserve unavailable repository provenance

--- src/Workflow/Transparency/TransparencyCategory.php
+++ src/Workflow/Transparency/TransparencyCategory.php
@@ -11,6 +11,7 @@ enum TransparencyCategory: string
 {
     case CHANGED_IN_SCOPE = 'changed_in_scope';
     case CHANGED_OUTSIDE_SCOPE = 'changed_outside_scope';
+    case REPOSITORY_OBSERVATION_UNAVAILABLE = 'repository_observation_unavailable';
     case CONTRACT_NON_GOAL = 'contract_non_goal';
     case CONTEXT_SKIPPED = 'context_skipped';
     case CONTEXT_OMITTED = 'context_omitted';
@@ -22,7 +23,9 @@ enum TransparencyCategory: string
     public function provenance(): TransparencyProvenance
     {
         return match ($this) {
-            self::CHANGED_IN_SCOPE, self::CHANGED_OUTSIDE_SCOPE => TransparencyProvenance::REPOSITORY_OBSERVATION,
+            self::CHANGED_IN_SCOPE,
+            self::CHANGED_OUTSIDE_SCOPE,
+            self::REPOSITORY_OBSERVATION_UNAVAILABLE => TransparencyProvenance::REPOSITORY_OBSERVATION,
             self::CONTRACT_NON_GOAL => TransparencyProvenance::WORKFLOW_AUTHORITY,
             self::CONTEXT_SKIPPED, self::CONTEXT_OMITTED => TransparencyProvenance::CONTEXT_CONSTRUCTION,
             self::REVIEW_FINDING => TransparencyProvenance::REVIEW_EVIDENCE,
