ae67c32e3628258e8116a298f6c2bb46681abda7
Lars Moelleken <lars@moelleken.org>
2026-08-27 03:12:11 +0200

    refactor(workflow): keep policy result passive

--- src/Run/RunPolicyEvaluation.php
+++ src/Run/RunPolicyEvaluation.php
@@ -18,17 +18,13 @@ final readonly class RunPolicyEvaluation
     public const string KIND_DECISION_REQUIRED = 'decision_required';
     public const string KIND_NONE = 'none';
 
-    public string $nextAction;
-
-    public string $nextActionKind;
-
     /** @param list<array{code: string, owner: string, message: string}> $blockers */
     public function __construct(
         public string $state,
         public bool $mutationAllowed,
         public bool $ordinaryCloseAllowed,
         public array $blockers,
-        string $nextAction,
+        public string $nextAction,
         /**
          * How the host must treat nextAction.
          *
@@ -42,24 +38,7 @@ final readonly class RunPolicyEvaluation
          *                     before the action can be executed
          * none              - nothing further is required
          */
-        string $nextActionKind = self::KIND_COMMAND,
+        public string $nextActionKind = self::KIND_COMMAND,
     ) {
-        $this->nextAction = $this->completeLearningTemplate($nextAction);
-        $this->nextActionKind = $nextActionKind;
-    }
-
-    private function completeLearningTemplate(string $nextAction): string
-    {
-        $learningTemplate = '--learning <no_durable_learning|findings_recorded|follow_up_required>'
-            . ' --learning-reason <learning-reason> --by <actor>';
-        if (!str_contains($nextAction, $learningTemplate)) {
-            return $nextAction;
-        }
-
-        return str_replace(
-            $learningTemplate,
-            $learningTemplate . ' [--finding <finding-id> ...]',
-            $nextAction,
-        );
     }
 }
