9e0cb0e0d5a87e2ce3c090fd8fdc258cea66dc36
Lars Moelleken <lars@moelleken.org>
2026-09-13 17:11:05 +0200

    fix(ci): align Recall owner floor guard

--- tests/RecallEditBriefingCompilerOwnerBoundaryTest.php
+++ tests/RecallEditBriefingCompilerOwnerBoundaryTest.php
@@ -33,6 +33,6 @@ final class RecallEditBriefingCompilerOwnerBoundaryTest extends TestCase
             JSON_THROW_ON_ERROR,
         );
         self::assertIsArray($composer);
-        self::assertSame('^0.17.6', $composer['require']['voku/agent-recall-compiler'] ?? null);
+        self::assertSame('^0.17.7', $composer['require']['voku/agent-recall-compiler'] ?? null);
     }
 }
